dc81742338cfb7b6d7caf484c645216d26345950
cuculeha <86766249+cuculeha@users.noreply.github.com>
2025-01-17 15:45:41 -0800

    second version

--- main.cpp
+++ main.cpp
@@ -1,6 +1,34 @@
+// Author      : Saliha Babar
+// Assignment  : A3
+// Due Date    : 01/22/2025
+// Purpose     : Standard Vector
+
 #include <iostream>
+#include <iomanip>
+
 using namespace std;
+
+const int numBooks = 20;
+float CalcTotal (const int quantity[], const float price[]);
 int main(){
-cout << "Hello this is assignment for vector array!" << endl;
+  int onHand [20] = {17,3,6,0,19,7,18,15,5,4,7,14,18,12,12,3,5,19,16,15};
+  float wholesale [20] = {72.01 , 40.15, 56.52, 31.61, 72.11, 71.84, 91.06,
+                          40.12, 22.27, 71.64, 24.26, 34.18, 44.92, 19.70,
+                          41.70, 66.42, 83.43, 53.72, 31.22, 98.19};
+  float total;
+
+  // Pass these data to the function
+  total = CalcTotal (onHand, wholesale);
+  cout << "The total is $" << total << endl;
 return 0;
+}
+
+float CalcTotal (const int quantity[], const float price[])
+{
+  float sum = 0;
+  for (int i= 0; i < 20 ; i++)
+    {
+      sum += quantity[i] * price[i]
+    }
+  return sum;
 }
