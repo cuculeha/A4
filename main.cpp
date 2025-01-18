0b34a226b8e2722f3f0e956660493fcb50bc7389
cuculeha <86766249+cuculeha@users.noreply.github.com>
2025-01-17 17:49:55 -0800

    new

--- main.cpp
+++ main.cpp
@@ -5,36 +5,61 @@
 
 #include <iostream>
 #include <iomanip>
-#include <cstring>
 
 using namespace std;
 
 const int numBooks = 20;
 
-float CalcTotal (const int quantity[], const float price[]);
+double CalcTotal (const int quantity[], const float price[]);
+void FormatOutput ( const double total );
 
 int main(){
   int onHand [20] = {17,3,6,0,19,7,18,15,5,4,7,14,18,12,12,3,5,19,16,15};
   float wholesale [20] = {72.01 , 40.15, 56.52, 31.61, 72.11, 71.84, 91.06,
                           40.12, 22.27, 71.64, 24.26, 34.18, 44.92, 19.70,
                           41.70, 66.42, 83.43, 53.72, 31.22, 98.19};
-  float total;
+  double total;
   string StringofTotal;
   
   // Pass these data to the function
   total = CalcTotal (onHand, wholesale);
-  
-  cout << fixed << setprecision(2) << "The total is $" << total << endl;
-  cout << "in string " << StringofTotal << endl;
+
+  // Format the output
+  FormatOutput (total);
 return 0;
 }
 
-float CalcTotal (const int quantity[], const float price[])
+double CalcTotal (const int quantity[], const float price[])
 {
-  float sum = 0;
+  double sum = 0;
   for (int i= 0; i < 20 ; i++)
     {
       sum += quantity[i] * price[i];
     }
   return sum;
+}
+
+
+void FormatOutput ( const double total )
+{
+  int sumTruncated = static_cast<int>(total);
+	int decimal;
+	int first;
+	int second;
+	int third;
+	
+	decimal = (total - sumTruncated) * 100 ;
+	first = sumTruncated % 1000;
+	second = (sumTruncated % 1000000)/1000;
+	third = (sumTruncated % 1000000000) /1000000;
+	
+	
+	cout << decimal << endl;
+	cout << first << endl;
+	cout << second << endl;
+	cout << third << endl;
+	
+	cout << "$" << third << "," << second << "," << first <<
+	"." << decimal << endl;
+	
 }
