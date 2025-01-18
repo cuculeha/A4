1fda56bbdcf33a94c28b6fc771f674f8c065b70d
cuculeha <86766249+cuculeha@users.noreply.github.com>
2025-01-17 16:14:35 -0800

    thirdversion

--- main.cpp
+++ main.cpp
@@ -5,22 +5,28 @@
 
 #include <iostream>
 #include <iomanip>
+#include <cstring>
 
 using namespace std;
 
 const int numBooks = 20;
+
 float CalcTotal (const int quantity[], const float price[]);
+
 int main(){
   int onHand [20] = {17,3,6,0,19,7,18,15,5,4,7,14,18,12,12,3,5,19,16,15};
   float wholesale [20] = {72.01 , 40.15, 56.52, 31.61, 72.11, 71.84, 91.06,
                           40.12, 22.27, 71.64, 24.26, 34.18, 44.92, 19.70,
                           41.70, 66.42, 83.43, 53.72, 31.22, 98.19};
   float total;
-
+  string StringofTotal;
+  
   // Pass these data to the function
   total = CalcTotal (onHand, wholesale);
+  StringofTotal = static_cast<string>(total);
   
   cout << fixed << setprecision(2) << "The total is $" << total << endl;
+  cout << "in string " << StringofTotal << endl;
 return 0;
 }
 
