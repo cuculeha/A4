27ec4310976e39a83cf3e510964e10142a9b02c1
cuculeha <86766249+cuculeha@users.noreply.github.com>
2025-01-22 15:29:33 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,11 +12,11 @@ using namespace std;
 
 const int numBooks = 20;
 
-double CalcTotal (const int quantity[], const float price[]);
+double CalcTotal (const vector<int> quantity, const float price[]);
 void FormatOutput ( double total );
 
 int main(){
-  vector <int > onHand {17,3,6,0,19,7,18,15,5,4,7,14,18,12,12,3,5,19,16,15};
+  vector <int> onHand {17,3,6,0,19,7,18,15,5,4,7,14,18,12,12,3,5,19,16,15};
   vector <float> wholesale = {72.01 , 40.15, 56.52, 31.61, 72.11, 71.84, 91.06,
                           40.12, 22.27, 71.64, 24.26, 34.18, 44.92, 19.70,
                           41.70, 66.42, 83.43, 53.72, 31.22, 98.19};
@@ -31,7 +31,7 @@ int main(){
 return 0;
 }
 
-double CalcTotal (const int quantity[], const float price[])
+double CalcTotal (const vector<int> quantity, const float price[]);
 {
   double sum = 0;
   for (int i= 0; i < 20 ; i++)
