724f72c5e0aab86db373b8296bbd39ae4e37b70d
cuculeha <86766249+cuculeha@users.noreply.github.com>
2025-01-22 17:44:44 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,7 +12,6 @@ using namespace std;
 const int numBooks = 20;
 
 double CalcTotal (const vector<int> &quantity, const vector <float> &price); // Function to calculate total
-void FormatOutput ( double total );                                          // Function to format output ( add commas & dollar sign )
 
 int main(){
   vector <int> onHand {17,3,6,0,19,7,18,15,5,4,7,14,18,12,12,3,5,19,16,15};
@@ -21,14 +20,12 @@ int main(){
                           41.70, 66.42, 83.43, 53.72, 31.22, 98.19};
   double total;
     
-  // Pass these data to the function
+ 	// Pass these data to the function
 	total = CalcTotal (onHand, wholesale);
+
+	// Format the output
  	std::cout.imbue(std::locale("en_US.UTF-8"));
 	cout << std:: showbase << std:: put_money(total * 100) << endl;
-
-
-  // Format the output
-  FormatOutput (total);
 
 return 0;
 }
@@ -41,37 +38,4 @@ double CalcTotal (const vector<int> &quantity,  const vector <float> &price)
       sum += quantity[i] * price[i];
     }
   return sum;
-}
-
-void FormatOutput ( double total )
-{
-	int sumTruncated = static_cast<int>(total);
-	int decimal;
-	int first;
-	int second;
-	int third;
-
-	decimal = (total - sumTruncated) * 100 ;
-	
-	// Assigning commas in their place
-	first = sumTruncated % 1000;
-	second = (sumTruncated % 1000000)/1000;
-	third = (sumTruncated % 1000000000) /1000000;
-
-	cout << "Inventory WholeSale Value : " ;
-	if (third!= 0 )
-	{
-		cout << "$" << third << "," << second << "," << first <<
-		"." << decimal << endl;
-	}
-	
-	else if (third == 0 )
-	{
-		cout << "$" << second << "," << first << "." << decimal << endl;
-	}
-
-	else if ( third == 0 && second == 0)
-	{
-		cout << "$" << first << "." << decimal << endl;
-	}
 }
